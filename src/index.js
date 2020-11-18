c3810d01a0c714bf4e15c238062cf5156f713e9f
DaliaMatus <daliamatus@gmail.com>
2020-11-18 00:30:52 -0600

    Configuré el servidor y conecté mi base de datos con mi mongodb local

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 const express=require('express');
-express();
+
+const config=require('./server/config');
+
+//Database
+require('./database');
+
+const app =config(express());
+
+//Starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
