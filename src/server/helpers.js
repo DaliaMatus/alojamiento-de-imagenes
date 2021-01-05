a242f5032e899d5c089efcfaa67fcbcc2b773cb7
DaliaMatus <daliamatus@gmail.com>
2021-01-05 05:11:55 -0600

    Limpie mi código quitando comentarios, cree la estructura para publicar un comentario y también cree mi modelo y mi controlador para poder guardar los comentarios en mi base de datos

--- src/server/helpers.js
+++ src/server/helpers.js
@@ -1,7 +1,7 @@
 const moment=require('moment');
 const helpers={};
 
-helpers.timeago=timestamp =>{//Crearemos una funcion (timeago) podemos nombrarlas como queramos, recibe el dato timestamp como parametro
-   return moment(timestamp).startOf('minute').fromNow();//A partir del tiempo que yo lo he publicado quiero saber cuanto ha pasado en minutos
+helpers.timeago=timestamp =>{
+   return moment(timestamp).startOf('minute').fromNow();
 }
     module.exports=helpers;
