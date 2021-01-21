2e46295de4f1b582bce90649416098ded98cfe65
DaliaMatus <daliamatus@gmail.com>
2021-01-20 21:24:12 -0600

    Cambié la dirección de mi base de datos, de local a MongoDB Atlas

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 module.exports={
     database:{
-        URI:'mongodb://localhost/imgshare'
+        URI:'mongodb+srv://Dalia_Matus:DeZQkwuFzVj9HZ8t@cluster0.mid5w.mongodb.net/imgshare?retryWrites=true&w=majority'
     }
 };
